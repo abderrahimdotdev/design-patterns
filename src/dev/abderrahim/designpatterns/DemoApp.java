8f60cb05bb605f370fcf5e4028a947ba34e9c2b9
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-27 00:11:14 +0100

    Add sample code in main() method to test Singleton pattern

--- src/dev/abderrahim/designpatterns/DemoApp.java
+++ src/dev/abderrahim/designpatterns/DemoApp.java
@@ -3,34 +3,57 @@ package dev.abderrahim.designpatterns;
 import dev.abderrahim.designpatterns.iterator.builtin.DinerMenu;
 import dev.abderrahim.designpatterns.iterator.custom.iterators.MenuIterator;
 import dev.abderrahim.designpatterns.iterator.custom.menu.BreakfastMenu;
+import dev.abderrahim.designpatterns.singleton.LazySingleton;
+import dev.abderrahim.designpatterns.singleton.Singleton;
 
 public class DemoApp {
     public static void main(String[] args) {
-        
-        /*
-         ************
-         * Iterator *
-         ************
-         */
 
-        // Using the custom Iterator interface:
-        BreakfastMenu breakfastMenu = new BreakfastMenu();
-        MenuIterator<String> it = breakfastMenu.createIterator();
+        {
+            /*
+             ************
+             * Iterator *
+             ************
+             */
 
-        while (it.hasNext()) {
-            String menuItem = it.next();
-            System.out.println(menuItem);
+            // Using the custom Iterator interface:
+            BreakfastMenu breakfastMenu = new BreakfastMenu();
+            MenuIterator<String> it = breakfastMenu.createIterator();
+
+            while (it.hasNext()) {
+                String menuItem = it.next();
+                System.out.println(menuItem);
+            }
+
+            // Using the builts-in Iterable and Iterator interfaces:
+            DinerMenu dinerMenu = new DinerMenu();
+
+            for (String menuItem : dinerMenu)
+                System.out.println(menuItem);
+
         }
+        {
+            /*
+             *************
+             * Singleton *
+             *************
+             */
 
-        System.out.println("------------------");
+            Singleton singleton = Singleton.getInstance();
+            Singleton singleton2 = Singleton.getInstance();
 
-        // Using the builts-in Iterable and Iterator interfaces:
-        DinerMenu dinerMenu = new DinerMenu();
+            System.out.println(singleton == singleton2); // OUTPUT: true
 
-        for (String menuItem : dinerMenu)
-            System.out.println(menuItem);
+            LazySingleton lazySingleton = LazySingleton.getInstance();
+            LazySingleton lazySingleton2 = LazySingleton.getInstance();
 
-        System.out.println("------------------");
+            System.out.println(lazySingleton == lazySingleton2); // OUTPUT: true
+
+        }
+        
+        {
+
+        }
 
     }
 }
