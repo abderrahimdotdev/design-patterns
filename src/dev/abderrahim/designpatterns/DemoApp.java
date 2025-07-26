c157eacce1e8d013bcaea97026198f9983f03045
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-26 20:05:45 +0100

    Initial commit

---
+++ src/dev/abderrahim/designpatterns/DemoApp.java
@@ -0,0 +1,7 @@
+package dev.abderrahim.designpatterns;
+
+public class DemoApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to Design Patterns Repo");
+    }
+}
