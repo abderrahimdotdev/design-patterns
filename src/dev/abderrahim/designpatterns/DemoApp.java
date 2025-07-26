af6eb146cdb7e5de90f5a339f20c54f55c27943f
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-26 20:20:07 +0100

    Add sample code in main() method to demonstrate the built-in Iterator

--- src/dev/abderrahim/designpatterns/DemoApp.java
+++ src/dev/abderrahim/designpatterns/DemoApp.java
@@ -1,17 +1,18 @@
 package dev.abderrahim.designpatterns;
 
+import dev.abderrahim.designpatterns.iterator.builtin.DinerMenu;
 import dev.abderrahim.designpatterns.iterator.custom.iterators.MenuIterator;
 import dev.abderrahim.designpatterns.iterator.custom.menu.BreakfastMenu;
 
 public class DemoApp {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        
         /*
          ************
          * Iterator *
          ************
          */
 
-        // Using a custom Iterator interface:
         BreakfastMenu breakfastMenu = new BreakfastMenu();
         MenuIterator<String> it = breakfastMenu.createIterator();
 
@@ -19,6 +20,15 @@ public class DemoApp {
             String menuItem = it.next();
             System.out.println(menuItem);
         }
+
+        System.out.println("------------------");
+
+        DinerMenu dinerMenu = new DinerMenu();
+
+        for (String menuItem : dinerMenu)
+            System.out.println(menuItem);
+
+        System.out.println("------------------");
 
     }
 }
