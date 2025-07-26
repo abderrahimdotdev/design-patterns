51bb01e0ee72f28a8b4cd3e4e707e38427db116b
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-26 20:14:53 +0100

    Add 1st test sample code in main() method

--- src/dev/abderrahim/designpatterns/DemoApp.java
+++ src/dev/abderrahim/designpatterns/DemoApp.java
@@ -1,7 +1,24 @@
 package dev.abderrahim.designpatterns;
 
+import dev.abderrahim.designpatterns.iterator.custom.iterators.MenuIterator;
+import dev.abderrahim.designpatterns.iterator.custom.menu.BreakfastMenu;
+
 public class DemoApp {
     public static void main(String[] args){
-        System.out.println("Welcome to Design Patterns Repo");
+        /*
+         ************
+         * Iterator *
+         ************
+         */
+
+        // Using a custom Iterator interface:
+        BreakfastMenu breakfastMenu = new BreakfastMenu();
+        MenuIterator<String> it = breakfastMenu.createIterator();
+
+        while (it.hasNext()) {
+            String menuItem = it.next();
+            System.out.println(menuItem);
+        }
+
     }
 }
