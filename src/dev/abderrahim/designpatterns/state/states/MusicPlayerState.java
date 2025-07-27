ece1b6c71f36d124fc3dcb84e325adeb35ea0b64
Abderrahim <abderrahimdotdev@gmail.com>
2025-07-27 14:18:26 +0100

    Add comment into MusicPlayerState class

--- src/dev/abderrahim/designpatterns/state/states/MusicPlayerState.java
+++ src/dev/abderrahim/designpatterns/state/states/MusicPlayerState.java
@@ -2,6 +2,7 @@ package dev.abderrahim.designpatterns.state.states;
 
 import dev.abderrahim.designpatterns.state.context.MusicPlayer;
 
+// State interface (could be an abstract class): Declares methods that concrete states should implement.
 public interface MusicPlayerState {
 
     void turnUpVolume();
